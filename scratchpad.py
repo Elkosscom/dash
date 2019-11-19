52bdd495c6835fa3f89c93044d8de3bc130cbc0e
elkosscom <elkosscombine@gmail.com>
2019-11-19 17:27:01 +0000

    finished currency data grabbing, working on analysing data

--- scratchpad.py
+++ scratchpad.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import currency_data
-
+import datetime
 
 currency_data.main()
 df = pd.read_csv(currency_data.filename,index_col=(0,1),header=0)
-print(df.loc[df.index == 'GBP'])
-# for base in df.index.levels[1]:
-#     df.loc[df.index.levels[1]==base].to_csv(f'test_{base}.csv')
+# print(df.loc[df.index.get_level_values(1)=='GBP'])
+for base in df.index.levels[1]:
+    df.loc[df.index.get_level_values(1)==base].to_csv(f'test_{base}.csv')
